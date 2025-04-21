cded56b95751ff897a8695a5dffa54265b1872a5
Nathan Lyson <n_lyson@yahoo.com>
2025-04-21 14:36:44 -0500

    Putting it at 16k

--- amplify/functions/imageLLMReview/handler.ts
+++ amplify/functions/imageLLMReview/handler.ts
@@ -30,7 +30,7 @@ export const handler = async (event: any): Promise<string> => {
       body: JSON.stringify({
         model: "gpt-4.1-mini",
         messages,
-        max_tokens: 128000,
+        max_tokens: 16000,
       }),
     });
 
