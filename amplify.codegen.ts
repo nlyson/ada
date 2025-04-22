b2186d590f07b437d148de82e7f5572c265c47ea
Nathan Lyson <nathan.lyson@gmail.com>
2025-04-21 20:53:59 -0500

    Removed codegen

--- amplify.codegen.ts
+++
@@ -1,7 +0,0 @@
-import { generateStorageClient } from '@aws-amplify/codegen/storage';
-import { storage } from './amplify/storage/resource'; // Your Gen 2 storage resource
-
-generateStorageClient({
-  storage,
-  outputPath: './src/amplify/storage.ts', // or just './amplify/storage.ts'
-});
