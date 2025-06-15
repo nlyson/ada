ee13383fead7a7d658557d22587ce49e35eaef92
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-15 18:28:26 -0500

    Using outputs

--- lib/configureAmplify.ts
+++ lib/configureAmplify.ts
@@ -1,15 +1,15 @@
-// lib/configureAmplify.ts
+import outputs from '@/amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
-import amplifyConfig from '@/amplify_outputs.json';
 
+// Configure with full outputs FIRST
+Amplify.configure(outputs);
 
+// THEN override just the storage bucket
 Amplify.configure({
-  ...amplifyConfig,
   Storage: {
     S3: {
       bucket: "picture-this-storage",
       region: "us-east-1",
     }
   }
-});
-
+}, { ssr: true });
