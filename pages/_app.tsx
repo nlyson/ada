e68c19ed4c0f4ddb21718fa91fa4cf26f5a398c1
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-15 17:27:50 -0500

    I dont' now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,5 @@
 import '@/lib/configureAmplify'; // This runs the Amplify.configure() automatically
 import { Amplify } from 'aws-amplify';
-import amplifyConfig from '@/amplify_outputs.json';
-
-Amplify.configure({
-  ...amplifyConfig,
-  Storage: {
-    S3: {
-      bucket: "picture-this-storage",
-      region: "us-east-1",
-    }
-  }
-});
-
-Amplify.configure({
-  Storage: {
-    S3: {
-      bucket: "picture-this-storage",
-      region: "us-east-1",
-    }
-  }
-});
 
 console.log('🔥 AFTER CONFIG:', typeof Amplify !== 'undefined' ? Amplify.getConfig() : 'Amplify not loaded');
 
