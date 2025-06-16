a94bcd5822737cd95252d6dadaccfe7693027df0
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-15 19:09:31 -0500

    finally i think

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,5 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-  Storage: {
-    S3: {
-      bucket: "picture-this-storage",  // ← Your correct bucket
-      region: "us-east-1",
-    }
-  }
-});
 
 
 export default function Home() {
