ad1d4797984da4fe9d0f35846920e6fa2b6e4c39
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-16 18:55:18 -0500

    Added indentiy pool

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -12,5 +12,6 @@ backend.addOutput({
     user_pool_id: "us-east-1_vin6qLM49",
     user_pool_client_id: "bhkb9c7knji0t123viqjompep",
     aws_region: "us-east-1",
+    identity_pool_id: "us-east-1:3cecf3e8-aec0-43e8-b35f-3bf181b6a540",
   },
 });
