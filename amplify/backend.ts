68def4005a971bc1631a8caa81cff9c24df03861
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-16 17:45:36 -0500

    Added auth

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -116,5 +116,10 @@ backend.addOutput({
     storage: {
       bucket_name: "picture-this-storage",
     },
+    auth: {
+      user_pool_id: "us-east-1_vin6qLM49",
+      user_pool_client_id: "bhkb9c7knji0t123viqjompep",
+      region: "us-east-1",
+    },
   },
 });
