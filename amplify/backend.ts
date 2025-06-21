105f88afe42e3d5eb5deb95b4339cab2f9e13c39
Nathan Lyson <nathan.lyson@gmail.com>
2025-06-20 19:37:03 -0500

    fixed backend login

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -14,5 +14,23 @@ backend.addOutput({
     user_pool_client_id: "bhkb9c7knji0t123viqjompep",
     aws_region: "us-east-1",
     identity_pool_id: "us-east-1:3cecf3e8-aec0-43e8-b35f-3bf181b6a540",
+    mfa_methods: [],
+    standard_required_attributes: [
+      "email"
+    ],
+    username_attributes: [],
+    user_verification_types: [
+      "email"
+    ],
+    groups: [],
+    mfa_configuration: "NONE",
+    password_policy: {
+      min_length: 8,
+      require_lowercase: true,
+      require_numbers: true,
+      require_symbols: true,
+      require_uppercase: true
+    },
+    "unauthenticated_identities_enabled": true
   },
 });
