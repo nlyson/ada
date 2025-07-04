2a41a578cb7573d58a4d1bb1de51e80819af85d9
Nathan Lyson <nathan.lyson@gmail.com>
2025-07-04 09:54:17 -0500

    Changed app name to no spacee

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.jamapantel.photomentor',
-  appName: 'Photo Mentor',
+  appName: 'PhotoMentor',
   webDir: 'out',
   server: {
     androidScheme: 'https',
