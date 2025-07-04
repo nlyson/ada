d1c14aa174f1a0759c031a67c3c72979287f6d6b
Nathan Lyson <nathan.lyson@gmail.com>
2025-07-04 09:57:44 -0500

    name to app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.jamapantel.photomentor',
-  appName: 'PhotoMentor',
+  appName: 'App',
   webDir: 'out',
   server: {
     androidScheme: 'https',
